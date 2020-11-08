d228dafc4aba7ce78dbf97431c9e12ad2c3ac002
pmkuria <pmkuria@safaricom.co.ke>
2020-11-08 11:59:44 +0300

    handle mode: Api and CMD

---
+++ assignment/cmd_mode.py
@@ -0,0 +1,8 @@
+from kcbfaqs import KcbFaqs
+
+if __name__ == '__main__':
+    KcbFaqs_instance = KcbFaqs('CMD')
+    kcb_faqs_data = KcbFaqs_instance.load_kcb_faqs_data('data/', 'KCB FAQs.xlsx')
+    intent_classes_data = KcbFaqs_instance.extra_intent_text_and_bi_grams(kcb_faqs_data)
+    input_text = input("Enter your KCB question: ")
+    KcbFaqs_instance.print_pretty(KcbFaqs_instance.classify_using_bi_grams_computation(input_text, intent_classes_data))
